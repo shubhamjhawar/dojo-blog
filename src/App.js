16988a4373cd4e5c4af65956ec587643ec926d01
Shubham Jhawar <jhawarshubhamcs@gmail.com>
2022-03-13 18:19:57 +0530

    --added code for the blogs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Navbar from './Navbar';
 import Home from './Home'
+
 function App() {
-  
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       <div className="content">
-         < Home />
+        < Home/>
       </div>
-      
     </div>
   );
 }
 
 export default App;
+
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,34 @@
-import {useState} from 'react';
+import { useState , useEffect } from "react";
+import BlogList from "./Bloglist";
 
 const Home = () => {
- 
-  const [blogs,setBlog] = useState([
+  const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
-  return (
-    <div className="home">
-      {blogs.map((blog) => (
-        <div className='blog-preview' key = {blog.id}>
-             <h2>{blog.title}</h2> 
-             <p>Written by {blog.author}</p>
-        </div>
+  ])
 
-      ))} 
-      
+  const [name,setName] = useState('Mario');
+
+  useEffect(() => {
+    console.log('Use effect ran'); 
+    console.log(name);
+  },[name]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  return ( 
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {/* <BlogList blogs={blogs.filter((blog) => 
+        blog.author === 'mario'
+      )} title="Mario Blogs" handleDelete={handleDelete} /> */}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
+
     </div>
   );
 }
